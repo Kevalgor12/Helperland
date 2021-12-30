c97d3a756d85ed63e4ce03f92f45636c4009ea5b
Keval Gor <kevalgor12@gmail.com>
2021-12-30 20:07:54 +0530

    About and Contact page complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,23 +17,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 50) {
-            $("home.navbar").addClass("navbar-bg-color");
-            // $(".navbar").css("background" , "#272727B3");
-            // $(".navbar").css("height" , "64px");
-            // $(".navbar").css("margin-top" , "0px");
-            // $(".navbar-brand img").css("width" , "72.69px");
-            // $(".navbar-brand img").css("height" , "54px");
-            // $(".navbar .navbar-nav .rounded-link").css("background" , "#005F7199");
+            $(".homepage").addClass("navbar-bg-color");
         }
   
         else{
-            $("home.navbar").removeClass("navbar-bg-color");
-            // $(".navbar").css("background" , "transparent");
-            // $(".navbar").css("height" , "130px");
-            // $(".navbar").css("margin-top" , "10px");
-            // $(".navbar-brand img").css("width" , "175px");
-            // $(".navbar-brand img").css("height" , "130px");
-            // $(".navbar .navbar-nav .rounded-link").css("background" , "transparent");
+            $(".homepage").removeClass("navbar-bg-color");
         }
     })
 })
