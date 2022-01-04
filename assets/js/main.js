bbee416e47f1bda90c2b70cc3bf73788b0b3181a
Keval Gor <kevalgor12@gmail.com>
2022-01-04 19:18:40 +0530

    Upcoming Service page commited.

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,35 @@ $(document).ready(function(){
         }
     })
 })
+
+$(document).ready(function () {
+    $('#table').DataTable({
+        paging: false,
+        bFilter: false,
+        ordering: true,
+        searching: false,
+        info: false,
+        "columnDefs": [
+            { "orderable": false, "targets": 4 }
+        ],
+        responsive: true
+    });
+
+    // $('#table').DataTable({
+    //     paging: true,
+    //     "pagingType": "full_numbers",
+    //     // bFilter: false,
+    //     ordering: true,
+    //     searching: false,
+    //     info: true,
+    //     "columnDefs": [
+    //         { "orderable": false, "targets": 4 }
+    //     ],
+    //     "oLanguage": {
+    //         "sInfo": "Total Records: TOTAL"
+    //     },
+    //     "dom": '<"top">rt<"bottom"lip><"clear">',
+    //     responsive: true
+    // });
+
+});
