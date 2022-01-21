d4c977edc39ad5a57db6f9a48a708a24bf1c24ed
Keval Gor <kevalgor12@gmail.com>
2022-01-21 16:08:27 +0530

    mysql schema uploaded.

---
+++ Database/helperland.sql
@@ -0,0 +1,263 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 21, 2022 at 11:22 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `helperland`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `AddressID` int(10) NOT NULL,
+  `StreetName` varchar(50) NOT NULL,
+  `HouseNumber` varchar(5) NOT NULL,
+  `PostalCode` varchar(10) NOT NULL,
+  `City` varchar(50) NOT NULL,
+  `PhoneNumber` varchar(20) NOT NULL,
+  `UserID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `extra_service`
+--
+
+CREATE TABLE `extra_service` (
+  `ExtraServiceID` int(10) NOT NULL,
+  `ExtraService` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `FeedbackID` int(10) NOT NULL,
+  `Feedback` varchar(500) NOT NULL,
+  `serviceproviderRating` varchar(10) NOT NULL,
+  `UserID` int(10) NOT NULL,
+  `ServiceProviderID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `PaymentID` int(10) NOT NULL,
+  `PaymentDate` datetime DEFAULT NULL,
+  `PaymentStatus` varchar(50) NOT NULL DEFAULT 'Pending',
+  `UserID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `serviceprovider_details`
+--
+
+CREATE TABLE `serviceprovider_details` (
+  `ServiceProviderID` int(10) NOT NULL,
+  `FirstName` varchar(100) NOT NULL,
+  `LastName` varchar(100) NOT NULL,
+  `MobileNumber` varchar(20) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_booking`
+--
+
+CREATE TABLE `service_booking` (
+  `ServiceID` int(10) NOT NULL,
+  `UserID` int(10) NOT NULL,
+  `ServiceProviderID` int(10) NOT NULL,
+  `ExtraServiceID` int(10) DEFAULT NULL,
+  `AddressID` int(10) NOT NULL,
+  `ExtraService` varchar(100) DEFAULT NULL,
+  `Address` varchar(500) NOT NULL,
+  `Zipcode` varchar(10) NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL,
+  `Status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_details`
+--
+
+CREATE TABLE `user_details` (
+  `UserID` int(10) NOT NULL,
+  `FirstName` varchar(100) NOT NULL,
+  `LastName` varchar(100) NOT NULL,
+  `MobileNumber` varchar(20) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`AddressID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `extra_service`
+--
+ALTER TABLE `extra_service`
+  ADD PRIMARY KEY (`ExtraServiceID`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`FeedbackID`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `ServiceProviderID` (`ServiceProviderID`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`PaymentID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `serviceprovider_details`
+--
+ALTER TABLE `serviceprovider_details`
+  ADD PRIMARY KEY (`ServiceProviderID`);
+
+--
+-- Indexes for table `service_booking`
+--
+ALTER TABLE `service_booking`
+  ADD PRIMARY KEY (`ServiceID`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `ServiceProviderID` (`ServiceProviderID`),
+  ADD KEY `ExtraServiceID` (`ExtraServiceID`),
+  ADD KEY `AddressID` (`AddressID`);
+
+--
+-- Indexes for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `AddressID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `extra_service`
+--
+ALTER TABLE `extra_service`
+  MODIFY `ExtraServiceID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `FeedbackID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `PaymentID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `serviceprovider_details`
+--
+ALTER TABLE `serviceprovider_details`
+  MODIFY `ServiceProviderID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service_booking`
+--
+ALTER TABLE `service_booking`
+  MODIFY `ServiceID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_details`
+--
+ALTER TABLE `user_details`
+  MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user_details` (`UserID`);
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user_details` (`UserID`),
+  ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`ServiceProviderID`) REFERENCES `serviceprovider_details` (`ServiceProviderID`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user_details` (`UserID`);
+
+--
+-- Constraints for table `service_booking`
+--
+ALTER TABLE `service_booking`
+  ADD CONSTRAINT `service_booking_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user_details` (`UserID`),
+  ADD CONSTRAINT `service_booking_ibfk_2` FOREIGN KEY (`ServiceProviderID`) REFERENCES `serviceprovider_details` (`ServiceProviderID`),
+  ADD CONSTRAINT `service_booking_ibfk_3` FOREIGN KEY (`ExtraServiceID`) REFERENCES `extra_service` (`ExtraServiceID`),
+  ADD CONSTRAINT `service_booking_ibfk_4` FOREIGN KEY (`AddressID`) REFERENCES `address` (`AddressID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
